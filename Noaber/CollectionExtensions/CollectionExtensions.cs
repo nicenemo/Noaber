fefb1c0c818b25918beb3c26ef04e97e7800f650
Hans Kruse <kruse@hanskruse.eu>
2013-08-17 06:46:09 +0200

    added unit tests for tuples of shorts and added Sum extension method for IEnumerable of short

---
+++ Noaber/CollectionExtensions/CollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+
+
+   public static partial class CollectionExtensions
+    {
+       public static short Sum(this IEnumerable<short> value ) {
+           if (value == null)
+           {
+               throw new ArgumentNullException("value", "value is null");
+           }
+           return value.Aggregate((short acc,short v) => (short)(acc+v));     
+       }
+
+    }
+
---
+++ Test.NoaberCS/TupleSumTests.Short.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+
+    public partial class TupleSumTests
+    {
+        [TestMethod]
+        public void ShortSumOnTwoTuple()
+        {
+            Tuple<short, short> sut = new Tuple<short, short>(1, 2);
+            short expected = sut.AsEnumerable().Cast<short>().Sum();
+            short actual = sut.Sum(x => (short)x);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShortSumOnThreeTuple()
+        {
+            Tuple<short, short, short> sut = new Tuple<short, short, short>(1, 2, 3);
+            short expected = sut.AsEnumerable().Cast<short>().Sum();
+            short actual = sut.Sum(x => (short)x);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShortSumOnFourTuple()
+        {
+
+            Tuple<short, short, short, short> sut = new Tuple<short, short, short, short>(1, 2, 3, 4);
+            short expected = sut.AsEnumerable().Cast<short>().Sum();
+            short actual = sut.Sum(x => (short)x);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShortSumOnFiveTuple()
+        {
+
+            Tuple<short, short, short, short, short> sut = new Tuple<short, short, short, short, short>(1, 2, 3, 4, 5);
+            short expected = sut.AsEnumerable().Cast<short>().Sum();
+            short actual = sut.Sum(x => (short)x);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShortSumOnSixTuple()
+        {
+
+            Tuple<short, short, short, short, short, short> sut = new Tuple<short, short, short, short, short, short>(1, 2, 3, 4, 5, 6);
+            short expected =sut.AsEnumerable().Cast<short>().Sum();
+            short actual = sut.Sum(x => (short)x);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShortSumOnSevenTuple()
+        {
+            Tuple<short, short, short, short, short, short, short> sut = new Tuple<short, short, short, short, short, short, short>(1, 2, 3, 4, 5, 6, 7);
+            short expected = sut.AsEnumerable().Cast<short>().Sum();
+            short actual = sut.Sum(x => (short)x);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
