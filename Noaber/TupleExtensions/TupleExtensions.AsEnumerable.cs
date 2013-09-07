ce28ab295dcf1480efaef8e770faee913f20d52c
Hans Kruse <kruse@hanskruse.eu>
2013-09-07 16:28:41 +0200

    removed while blocks

--- Noaber/TupleExtensions/TupleExtensions.AsEnumerable.cs
+++ Noaber/TupleExtensions/TupleExtensions.AsEnumerable.cs
@@ -14,71 +14,43 @@ public static partial class TupleExtensions
   public static IEnumerable AsEnumerable<T>(this Tuple<T> value)
   { 
     int i = -1;
-    while (++i < value.Length())
-    {
-      object result = value.Item(i);
-      yield return result;
-    }
-  }
+    while (++i < value.Length()) yield return value.Item(i);
+   }
 
   public static IEnumerable AsEnumerable<T1, T2>(this Tuple<T1, T2> value)
   {
     int i = -1;
-    while (++i < value.Length())
-    {
-      object result = value.Item(i);
-      yield return result;
-    }
-  }
+    while (++i < value.Length()) yield return value.Item(i);
+   }
 
   public static IEnumerable AsEnumerable<T1, T2, T3>(this Tuple<T1, T2, T3> value)
   {
     int i = -1;
-    while (++i < value.Length())
-    {
-      object result = value.Item(i);
-      yield return result;
-    }
-  }
+    while (++i < value.Length()) yield return value.Item(i);
+   }
 
   public static IEnumerable AsEnumerable<T1, T2, T3, T4>(this Tuple<T1, T2, T3, T4> value)
   {
     int i = -1;
-    while (++i < value.Length())
-    {
-      object result = value.Item(i);
-      yield return result;
-    }
-  }
+    while (++i < value.Length()) yield return value.Item(i);
+   }
 
 
   public static IEnumerable AsEnumerable<T1, T2, T3, T4, T5>(this Tuple<T1, T2, T3, T4, T5> value)
   {
     int i = -1;
-    while (++i < value.Length())
-    {
-      object result = value.Item(i);
-      yield return result;
-    }
+    while (++i < value.Length()) yield return value.Item(i);
   }
 
   public static IEnumerable AsEnumerable<T1, T2, T3, T4, T5, T6>(this Tuple<T1, T2, T3, T4, T5, T6> value)
   {
     int i = -1;
-    while (++i < value.Length())
-    {
-      object result = value.Item(i);
-      yield return result;
-    }
+    while (++i < value.Length()) yield return value.Item(i);
   }
 
   public static IEnumerable AsEnumerable<T1, T2, T3, T4, T5, T6, T7>(this Tuple<T1, T2, T3, T4, T5, T6, T7> value)
   {
     int i = -1;
-    while (++i < value.Length())
-    {
-      object result = value.Item(i);
-      yield return result;
-    }
+    while (++i < value.Length()) yield return value.Item(i);
   }
 }
